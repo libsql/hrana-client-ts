cc458bd2c468715f96d53c88609d100cee8c6fe2
Jan Špaček <patek.mail@gmail.com>
2023-03-10 11:45:08 +0100

    Reshuffle result.ts

--- src/result.ts
+++ src/result.ts
@@ -3,6 +3,44 @@ import type { Value } from "./value.js";
 import { valueFromProto } from "./value.js";
 import type * as proto from "./proto.js";
 
+/** Result of executing a database statement. */
+export interface StmtResult {
+    /** Number of rows that were changed by the statement. This is meaningful only if the statement was an
+     * INSERT, UPDATE or DELETE, and the value is otherwise undefined. */
+    affectedRowCount: number;
+    /** The ROWID of the last successful insert into a rowid table. This is a 64-big signed integer encoded as
+     * a string. For other statements than INSERTs into a rowid table, the value is not specified. */
+    lastInsertRowid: string | undefined;
+    /** Names of columns in the result. */
+    columnNames: Array<string | undefined>;
+}
+
+/** An array of rows returned by a database statement. */
+export interface RowsResult extends StmtResult {
+    /** The returned rows. */
+    rows: Array<Row>;
+}
+
+/** A single row returned by a database statement. */
+export interface RowResult extends StmtResult {
+    /** The returned row. If the query produced zero rows, this is `undefined`. */
+    row: Row | undefined,
+}
+
+/** A single value returned by a database statement. */
+export interface ValueResult extends StmtResult {
+    /** The returned value. If the query produced zero rows or zero columns, this is `undefined`. */
+    value: Value | undefined,
+}
+
+/** Row returned from the database. This is an Array-like object (it has `length` and can be indexed with a
+ * number), and in addition, it has enumerable properties from the named columns. */
+export interface Row {
+    length: number;
+    [index: number]: Value;
+    [name: string]: Value;
+}
+
 export function stmtResultFromProto(result: proto.StmtResult): StmtResult {
     return {
         affectedRowCount: result["affected_row_count"],
@@ -51,45 +89,6 @@ function rowFromProto(colNames: Array<string | undefined>, values: Array<proto.V
     return row as Row;
 }
 
-/** Result of executing a database statement. */
-export interface StmtResult {
-    /** Number of rows that were changed by the statement. This is meaningful only if the statement was an
-     * INSERT, UPDATE or DELETE, and the value is otherwise undefined. */
-    affectedRowCount: number;
-    /** The ROWID of the last successful insert into a rowid table. This is a 64-big signed integer encoded as
-     * a string. For other statements than INSERTs into a rowid table, the value is not specified. */
-    lastInsertRowid: string | undefined;
-    /** Names of columns in the result. */
-    columnNames: Array<string | undefined>;
-}
-
-/** An array of rows returned by a database statement. */
-export interface RowsResult extends StmtResult {
-    /** The returned rows. */
-    rows: Array<Row>;
-}
-
-/** A single row returned by a database statement. */
-export interface RowResult extends StmtResult {
-    /** The returned row. If the query produced zero rows, this is `undefined`. */
-    row: Row | undefined,
-}
-
-/** A single value returned by a database statement. */
-export interface ValueResult extends StmtResult {
-    /** The returned value. If the query produced zero rows or zero columns, this is `undefined`. */
-    value: Value | undefined,
-}
-
-/** Row returned from the database. This is an Array-like object (it has `length` and can be indexed with a
- * number), and in addition, it has enumerable properties from the named columns. */
-export interface Row {
-    length: number;
-    [index: number]: Value;
-    [name: string]: Value;
-}
-
 export function errorFromProto(error: proto.Error): ResponseError {
     return new ResponseError(error["message"], error);
 }
-
