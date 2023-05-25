85d0c1d24879a5a4df314951f5cce08376110e2f
Jan Špaček <patek.mail@gmail.com>
2023-05-25 10:13:16 +0200

    Handle both WS and HTTP URLs in parseLibsqlUrl()

--- src/__tests__/libsql_url.ts
+++ src/__tests__/libsql_url.ts
@@ -2,7 +2,9 @@ import * as hrana from "..";
 
 describe("parseLibsqlUrl()", () => {
     function expectParse(url: string, parsed: Partial<hrana.ParsedLibsqlUrl>) {
-        parsed.authToken = parsed.authToken ?? undefined;
+        parsed.hranaWsUrl ??= undefined;
+        parsed.hranaHttpUrl ??= undefined;
+        parsed.authToken ??= undefined;
         expect(hrana.parseLibsqlUrl(url)).toStrictEqual(parsed);
     }
 
@@ -11,32 +13,67 @@ describe("parseLibsqlUrl()", () => {
     }
 
     test("ws/wss URL", () => {
-        expectParse("ws://localhost", {hranaUrl: "ws://localhost"});
-        expectParse("ws://localhost:8080", {hranaUrl: "ws://localhost:8080"});
-        expectParse("ws://127.0.0.1:8080", {hranaUrl: "ws://127.0.0.1:8080"});
-        expectParse("ws://[2001:db8::1]:8080", {hranaUrl: "ws://[2001:db8::1]:8080"});
-        expectParse("wss://localhost", {hranaUrl: "wss://localhost"});
+        expectParse("ws://localhost", {hranaWsUrl: "ws://localhost"});
+        expectParse("ws://localhost:8080", {hranaWsUrl: "ws://localhost:8080"});
+        expectParse("ws://127.0.0.1:8080", {hranaWsUrl: "ws://127.0.0.1:8080"});
+        expectParse("ws://[2001:db8::1]:8080", {hranaWsUrl: "ws://[2001:db8::1]:8080"});
+        expectParse("ws://localhost/some/path", {hranaWsUrl: "ws://localhost/some/path"});
+        expectParse("wss://localhost", {hranaWsUrl: "wss://localhost"});
+    });
+
+    test("http/https URL", () => {
+        expectParse("http://localhost", {hranaHttpUrl: "http://localhost"});
+        expectParse("http://localhost/some/path", {hranaHttpUrl: "http://localhost/some/path"});
+        expectParse("https://localhost", {hranaHttpUrl: "https://localhost"});
     });
 
     test("libsql URL", () => {
-        expectParse("libsql://localhost", {hranaUrl: "wss://localhost"});
-        expectParse("libsql://localhost:8080", {hranaUrl: "wss://localhost:8080"});
+        expectParse("libsql://localhost", {
+            hranaWsUrl: "wss://localhost",
+            hranaHttpUrl: "https://localhost",
+        });
+        expectParse("libsql://localhost:8080", {
+            hranaWsUrl: "wss://localhost:8080",
+            hranaHttpUrl: "https://localhost:8080",
+        });
+        expectParse("libsql://localhost/some/path", {
+            hranaWsUrl: "wss://localhost/some/path",
+            hranaHttpUrl: "https://localhost/some/path",
+        });
+    });
+
+    test("tls disabled", () => {
+        expectParse("ws://localhost?tls=0", {hranaWsUrl: "ws://localhost"});
+        expectParse("http://localhost?tls=0", {hranaHttpUrl: "http://localhost"});
+        expectParseError("wss://localhost?tls=0", /tls=0/);
+        expectParseError("https://localhost?tls=0", /tls=0/);
+        expectParse("libsql://localhost:8080?tls=0", {
+            hranaWsUrl: "ws://localhost:8080",
+            hranaHttpUrl: "http://localhost:8080",
+        });
+        expectParseError("libsql://localhost?tls=0", /tls=0.* explicit port/);
+    });
+
+    test("tls enabled", () => {
+        expectParse("wss://localhost?tls=1", {hranaWsUrl: "wss://localhost"});
+        expectParse("https://localhost?tls=1", {hranaHttpUrl: "https://localhost"});
+        expectParseError("ws://localhost?tls=1", /tls=1/);
+        expectParseError("http://localhost?tls=1", /tls=1/);
+        expectParse("libsql://localhost?tls=1", {
+            hranaWsUrl: "wss://localhost",
+            hranaHttpUrl: "https://localhost",
+        });
     });
 
     test("authToken in query params", () => {
         expectParse("wss://localhost?authToken=foobar", {
-            hranaUrl: "wss://localhost",
+            hranaWsUrl: "wss://localhost",
             authToken: "foobar",
         });
     });
 
     test("unknown query param", () => {
         expectParseError("ws://localhost?foo", /"foo"/);
-    });
-
-    test("http/https scheme", () => {
-        expectParseError("http://localhost", /"http:".*"ws:"/);
-        expectParseError("https://localhost", /"https:".*"wss:"/);
     });
 
     test("unknown scheme", () => {
--- src/libsql_url.ts
+++ src/libsql_url.ts
@@ -1,11 +1,16 @@
 import { LibsqlUrlParseError } from "./errors.js";
 
-/** Result of parsing a libsql URL (using {@link parseLibsqlUrl()}). */
-export type ParsedLibsqlUrl = {
-    /** The URL which can be passed to {@link open()} to open a {@link Client}. */
-    hranaUrl: string,
-    /** The optional `authToken` query parameter that should be passed as `jwt` to {@link open()}. */
-    authToken: string | undefined,
+/** Result of parsing a libsql URL using {@link parseLibsqlUrl}. */
+export interface ParsedLibsqlUrl {
+    /** A WebSocket URL that can be used with {@link openWs} to open a {@link WsClient}. It is `undefined`
+     * if the parsed URL specified HTTP explicitly. */
+    hranaWsUrl: string | undefined;
+    /** A HTTP URL that can be used with {@link openHttp} to open a {@link HttpClient}. It is `undefined`
+     * if the parsed URL specified WebSockets explicitly. */
+    hranaHttpUrl: string | undefined;
+    /** The optional `authToken` query parameter that should be passed as `jwt` to
+     * {@link openWs}/{@link openHttp}. */
+    authToken: string | undefined;
 };
 
 /** Parses a URL compatible with the libsql client (`@libsql/client`). This URL may have the "libsql:" scheme
@@ -14,31 +19,57 @@ export function parseLibsqlUrl(urlStr: string): ParsedLibsqlUrl {
     const url = new URL(urlStr);
 
     let authToken: string | undefined = undefined;
+    let tls: boolean | undefined = undefined;
     for (const [key, value] of url.searchParams.entries()) {
         if (key === "authToken") {
             authToken = value;
+        } else if (key === "tls") {
+            if (value === "0") {
+                tls = false;
+            } else if (value === "1") {
+                tls = true;
+            } else {
+                throw new LibsqlUrlParseError(
+                    `Unknown value for the "tls" query argument: ${JSON.stringify(value)}`,
+                );
+            }
         } else {
             throw new LibsqlUrlParseError(`Unknown URL query argument ${JSON.stringify(key)}`);
         }
     }
 
-    let hranaScheme: string;
-    if (url.protocol === "http:") {
+    let hranaWsScheme: string | undefined;
+    let hranaHttpScheme: string | undefined;
+
+    if ((url.protocol === "http:" || url.protocol === "ws:") && (tls === true)) {
         throw new LibsqlUrlParseError(
-            'This client does not support "http:" URLs. Please use a "ws:" URL instead.'
+            `A ${JSON.stringify(url.protocol)} URL cannot opt into TLS using ?tls=1`,
         );
-    } else if (url.protocol === "https:") {
+    } else if ((url.protocol === "https:" || url.protocol === "wss:") && (tls === false)) {
         throw new LibsqlUrlParseError(
-            'This client does not support "https:" URLs. Please use a "wss:" URL instead.'
+            `A ${JSON.stringify(url.protocol)} URL cannot opt out of TLS using ?tls=0`,
         );
-    } else if (url.protocol === "libsql:") {
-        hranaScheme = "wss:";
+    }
+
+    if (url.protocol === "http:" || url.protocol === "https:") {
+        hranaHttpScheme = url.protocol;
     } else if (url.protocol === "ws:" || url.protocol === "wss:") {
-        hranaScheme = url.protocol;
+        hranaWsScheme = url.protocol;
+    } else if (url.protocol === "libsql:") {
+        if (tls === false) {
+            if (!url.port) {
+                throw new LibsqlUrlParseError(`A "libsql:" URL with ?tls=0 must specify an explicit port`);
+            }
+            hranaHttpScheme = "http:";
+            hranaWsScheme = "ws:";
+        } else {
+            hranaHttpScheme = "https:";
+            hranaWsScheme = "wss:";
+        }
     } else {
         throw new LibsqlUrlParseError(
             `This client does not support ${JSON.stringify(url.protocol)} URLs. ` +
-                'Please use a "libsql:", "ws:" or "wss:" URL instead.'
+                'Please use a "libsql:", "ws:", "wss:", "http:" or "https:" URL instead.'
         );
     }
 
@@ -57,6 +88,9 @@ export function parseLibsqlUrl(urlStr: string): ParsedLibsqlUrl {
         hranaPath = "";
     }
 
-    const hranaUrl = `${hranaScheme}//${url.host}${hranaPath}`;
-    return { hranaUrl, authToken };
+    const hranaWsUrl = hranaWsScheme !== undefined
+        ? `${hranaWsScheme}//${url.host}${hranaPath}` : undefined;
+    const hranaHttpUrl = hranaHttpScheme !== undefined
+        ? `${hranaHttpScheme}//${url.host}${hranaPath}` : undefined;
+    return { hranaWsUrl, hranaHttpUrl, authToken };
 }
